24096817aa4d44a4c36f07bf05cd56714d382b7a
Samuel Rowan <samuel@samuelrowan.com>
2019-10-25 17:58:49 -0500

    unfinished mess

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { numbers, operators, specials } from "./data.js";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
-
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators.js";
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers.js";
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials.js";
+import Display from "./components/DisplayComponents/Display.js"
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
 
@@ -18,6 +22,10 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Display />
+        <Operators />
+        <Numbers />
+        <Specials />
       </div>
     </div>
   );
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = (props) => {
+  const {specialText} = props;
   return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    <button>
+      {props.button}
+    </button>
   );
 };
+export default SpecialButton;
